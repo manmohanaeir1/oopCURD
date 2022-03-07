f044d94909a7ff488d8e93c23ee18fb2ac951572
manmohanaeir1 <manmohanaeir001110@gmail.com>
2022-03-07 14:10:28 +0545

    insert data using oop methods:

--- database.php
+++ database.php
@@ -14,6 +14,7 @@
         public function __construct(){
             if(!$this->conn) {
                 $this->mysqli = new mysqli($this->db_host,$this->db_user,$this->db_pass,$this->db_name);
+                $this->conn = true;
                 if($this->mysqli->connect_errno) {
                    array_push($this->result,$this->mysqli->connect_errno);
                    return false;
@@ -24,8 +25,23 @@
 
         }
         // for inserting data
-        public function insert(){
+        public function insert($table ,$parames = array()){
+                 if($this->tableExist($table)){
+                    $table_colms = implode(', ', array_keys($parames));
+                    $table_value = implode("', '", $parames);
 
+                    $sql = "INSERT INTO  $table ($table_colms) VALUES ('$table_value')";
+
+                    if($this->mysqli->query(($sql))){
+                        array_push($this->result, $this->mysqli->insert_id);
+                        return true;
+                    }else{
+                        array_push($this->result, $this->mysqli->error);
+                        return false;
+                    }
+                 }else{
+                    return false;
+                 }
         }
 
         // update row data
@@ -37,6 +53,26 @@
         //delete data from Db
         public function delete(){
 
+        }
+
+        private function tableExist($table){
+            $sql = "SHOW TABLES FROM $this->db_name LIKE '$table'";
+            $tblInDb = $this->mysqli->query($sql);
+            if($tblInDb){
+                if($tblInDb->num_rows ==1)
+                {
+                    return true;
+                }else{
+                    array_push($this->result, $table. "Does not exist in this database. ");
+                    return false;
+                }
+            }
+        }
+
+        public function getResult(){
+            $val = $this->result;
+            $this->result = array();
+            return $val;
         }
 
         // for close database connection
--- index.php
+++ index.php
@@ -2,4 +2,7 @@
     include 'database.php';
     
     $obj =new database();
+    $obj->insert('students', ['name'=>'Hari', 'age'=>19, 'city'=>'KTM']);
+    echo "insert  result is";
+    print_r($obj->getResult());
 ?>
